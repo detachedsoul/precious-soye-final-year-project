b8c3844adac4aaca233b77e3a657e480229f266e
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2022-06-03 10:31:13 +0100

    chore: carried out minor redesigns

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,6 @@
-// Variable declarations
 let menuToggle = document.querySelector(".menu-toggle");
 let nav = document.querySelector("nav");
-let scrollToTopBtn = document.querySelector(".scroll-to-top");
-
-// window.onscroll = () => {
-// 	if (
-// 		document.documentElement.scrollTop >= 300 ||
-// 		document.body.scrollTop >= 300
-// 	) {
-// 		scrollToTopBtn.classList.remove("translate-x-[180%]");
-// 	} else {
-// 		scrollToTopBtn.classList.add("translate-x-[180%]");
-// 	}
-// };
 
 menuToggle.addEventListener("click", () => {
 	nav.classList.toggle("scale-0");
-	console.log("clicked", nav);
 });
-
-// scrollToTopBtn.addEventListener("click", () => {
-//     scrollTo({
-// 		top: 0,
-// 		left: 0,
-// 		behavior: "smooth",
-// 	});
-// })
